c0925e6f0a0868850e9c3b18d5b9eff3b53db63f
Alex <regulasalexandru@yahoo.com>
2021-11-04 20:19:56 +0200

    class cat

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-public class Main {
+import java.util.*;
+class Cat{
+    private String name;
+    private int age;
+
+    public Cat(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString(){
+        return name + " " + age;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("negrutu", 1);
+        Cat cat2 = new Cat("griutu", 2);
+        System.out.println(cat1.toString());
+        System.out.println(cat2.toString());
+
+    }
 }
