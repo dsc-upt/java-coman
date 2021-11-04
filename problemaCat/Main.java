613e925e13d6433e0f434410b3c8f988fe5c9168
Tudor <tudor.androne@student.upt.ro>
2021-11-04 20:09:17 +0200

    replaced classes

---
+++ problemaCat/Main.java
@@ -0,0 +1,13 @@
+package gdsc1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Cat cat1 = new Cat("N1", 2);
+		Cat cat2 = new Cat("N2", 3);
+		System.out.println(cat1.getAge());
+		System.out.println(cat2.getAge());
+	}
+
+}
